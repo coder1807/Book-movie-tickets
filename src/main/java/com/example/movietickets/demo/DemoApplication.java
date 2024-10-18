8f96ab730fbc02846105704cadeb76196e67a244
TLong_1512 <125524778+ThanhLong1512@users.noreply.github.com>
2024-10-18 14:45:08 +0700

    test

--- src/main/java/com/example/movietickets/demo/DemoApplication.java
+++ src/main/java/com/example/movietickets/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example.movietickets.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.paypal.base.rest.APIContext")
+//@ComponentScan("org.springframework.security.config.annotation.web.builders.HttpSecurity")
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/movietickets/demo/config/PaypalConfig.java
@@ -0,0 +1,19 @@
+//package com.example.movietickets.demo.config;
+//
+//import com.paypal.base.rest.APIContext;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class PaypalConfig {
+//    @Value("${paypal.client-id}")
+//    private String clientID;
+//    @Value("${paypal.client-secret}")
+//    private String clientSecret;
+//    @Value("${paypal.mode}")
+//    private String mode;
+//
+//    public APIContext apiContext(){
+//        return new APIContext(clientID, clientSecret,mode);
+//    }
+//}
